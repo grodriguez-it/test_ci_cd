dcd9913c30c5f0f9fb7135562e840c646c4315ca
Gustavo Rodriguez <grodriguez.itnetwork@gmail.com>
2025-07-05 21:37:04 -0500

    VM 2 branch Test1

---
+++ deploy2.sh
@@ -0,0 +1,19 @@
+BRANCH=$1
+
+if [ "$BRANCH" == "main" ]; then
+    DEPLOY_PATH="/home/grodriguez/github/main"
+else
+    DEPLOY_PATH="/home/grodriguez/github/stage"
+fi
+
+$VM_SSH << REMOTE_SCRIPT
+
+# 1. Entrar al respectivo directorio
+rm -r $DEPLOY_PATH 
+cd $PROJECT_DIR
+
+# 2. Clonar repositorio
+git clone https://github.com/grodriguez-it/test_ci_cd.git .
+git pull --rebase origin $BRANCH
+
+REMOTE_SCRIPT
