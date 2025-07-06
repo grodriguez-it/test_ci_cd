37df36674054a86cb94f10bc2d1ae6525e01e303
Gustavo Rodriguez <grodriguez.itnetwork@gmail.com>
2025-07-05 19:49:25 -0500

    VM Test sh update1

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,24 @@
-ssh -o StrictHostKeyChecking=no grodriguez@4.246.203.10 << EOF
+$VM_SSH << REMOTE_SCRIPT
+mkdir $PROJECT_DIR
+cd $PROJECT_DIR || exit
 
-cd /home/azureuser/practicas-ga
-git pull --rebase origin main
+# Verifica si es un repo Git y si está conectado a un remoto
+if [ -d .git ]; then
+    echo "✔ Directorio ya es un repositorio Git"
+    if git remote -v | grep -q origin; then
+        echo "✔ Tiene remoto 'origin' configurado"
+    else
+        echo "⚠ Falta remoto: agregando..."
+        git remote add origin https://https://github.com/grodriguez-it/test_ci_cd.git
+    fi
+else
+    echo "✖ Inicializando nuevo repo Git..."
+    git init
+    git remote add origin https://https://github.com/grodriguez-it/test_ci_cd.git
+    git fetch
+    git checkout -b main
+fi
 
-EOF
+# Continúa con tu despliegue...
+git pull origin main
+REMOTE_SCRIPT
