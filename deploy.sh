924aefef7253e22eaa62e2a51eeb32f2cd73f796
Gustavo Rodriguez <grodriguez.itnetwork@gmail.com>
2025-07-05 20:17:23 -0500

    VM Test sh update4

--- deploy.sh
+++ deploy.sh
@@ -3,11 +3,12 @@ bash -c "$VM_SSH << 'REMOTE_SCRIPT'
   mkdir -p $PROJECT_DIR
   cd $PROJECT_DIR || exit
   
-  if [ -d .git ]; then
-    git pull origin main
-  else
-    git init
-    git remote add origin https://github.com/grodriguez-it/test_ci_cd.git
-    git fetch && git checkout -b main
-  fi
+# Forzar clonación (elimina el directorio si existe)
+if [ -d .git ]; then
+  git fetch --all
+  git reset --hard origin/main || git reset --hard origin/master
+else
+  git clone --branch main https://github.com/grodriguez-it/test_ci_cd.git .
+fi
+
 REMOTE_SCRIPT"
